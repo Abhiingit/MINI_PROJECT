d6053932b314f4e58d2162181e871ea98f778d8e
Abhiingit <abhijeetsingh5426@gmail.com>
2026-04-22 10:41:14 +0530

    Delete attendance-saas/config directory

--- attendance-saas/config/routes/leaveRoutes.js
+++
@@ -1,9 +0,0 @@
-const express         = require("express");
-const router          = express.Router();
-const auth            = require("../middleware/authMiddleware");
-const leaveController = require("../controllers/leaveController");
-
-router.post("/apply",  auth, leaveController.applyLeave);
-router.get("/my",      auth, leaveController.getMyLeaves);   // NEW
-
-module.exports = router;
