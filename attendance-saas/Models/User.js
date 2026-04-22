b96e61c940a5f4ad15836c8aa5e1aef26423c6fe
Abhiingit <abhijeetsingh5426@gmail.com>
2026-04-22 10:45:24 +0530

    Add files via upload

---
+++ attendance-saas/Models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+
+  name: String,
+
+  email: {
+    type: String,
+    unique: true
+  },
+
+  password: String,
+
+  role: {
+    type: String,
+    enum: ["admin", "manager", "employee", "student"],
+    default: "student"
+  },
+
+  companyId: String,
+  department: String
+
+});
+
+module.exports = mongoose.model("User", userSchema);
